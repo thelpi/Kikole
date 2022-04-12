f9326e9aefe5fabc4f121d0fd4ba6781ecfda338
Lpi <thelpi.v3@gmail.com>
2022-04-12 22:44:07 +0200

    mutualize JS

--- KikoleSite/wwwroot/js/site.js
+++ KikoleSite/wwwroot/js/site.js
@@ -1,4 +1,50 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿/* positions autovalidation */
+$(function () {
+    $("#positionSubmission").on("change", function (e) {
+        if ($("#positionSubmission").val() != "0") {
+            $("#submitPosition").click();
+        }
+    });
+});
+/* years autocompletion */
+$(function () {
+    var availableTags = [];
+    for (var i = 1850; i <= 2010; i++)
+        availableTags.push(i.toString());
+    $("#birthYearValue").autocomplete({
+        source: availableTags,
+        select: function (e, i) {
+            $("#birthYearValue").val(i.item.value);
+            if ($("#submitYear").length > 0) {
+                $("#submitYear").click();
+            }
+        },
+        minLength: 1
+    });
+});
+/* countries autocompletion */
+$(function () {
+    $("#countryName").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: '/Home/AutoCompleteCountries/',
+                data: {
+                    "prefix": request.term
+                },
+                type: "POST",
+                success: function (data) {
+                    response($.map(data, function (item) {
+                        return item;
+                    }))
+                }
+            });
+        },
+        select: function (e, i) {
+            $("#countryId").val(i.item.key);
+            if ($("#submitCountry").length > 0) {
+                $("#submitCountry").click();
+            }
+        },
+        minLength: 1
+    });
+});
