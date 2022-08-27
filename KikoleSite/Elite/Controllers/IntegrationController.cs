2d248ae29a5a469b9688654386c8b05c281a18e0
Lpi <thelpi.v3@gmail.com>
2022-08-27 23:37:59 +0200

    Finish integration controller

---
+++ KikoleSite/Elite/Controllers/IntegrationController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using KikoleSite.Elite.Enums;
+using KikoleSite.Elite.Providers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace KikoleSite.Elite.Controllers
+{
+    [Route("the-elite")]
+    public class IntegrationController : Controller
+    {
+        private readonly IIntegrationProvider _integrationProvider;
+        private readonly string _secret;
+
+        private static readonly object _lock = new object();
+        private static Task _runningTask;
+        private static object _taskResult;
+        private static string _taskName;
+
+        public IntegrationController(IConfiguration configuration, IIntegrationProvider integrationProvider)
+        {
+            _integrationProvider = integrationProvider;
+            _secret = configuration.GetValue<string>("TheEliteIntegrationSecret");
+        }
+
+        [HttpGet("players/refresh")]
+        public JsonResult RefreshPlayers([FromQuery] bool addTimesForNewPlayers, [FromQuery] string secret)
+        {
+            var json = StartAsyncTaskAndGetJson(() =>
+                _integrationProvider.RefreshPlayersAsync(addTimesForNewPlayers),
+                nameof(RefreshPlayers),
+                secret);
+            return Json(json);
+        }
+
+        [HttpGet("entries/{game}/refresh")]
+        public JsonResult RefreshGameEntries([FromRoute] Game game, [FromQuery] string secret)
+        {
+            var json = StartAsyncTaskAndGetJson(() =>
+                _integrationProvider.RefreshAllEntriesAsync(game),
+                nameof(RefreshGameEntries),
+                secret);
+            return Json(json);
+        }
+
+        [HttpGet("entries/refresh")]
+        public JsonResult RefreshRecentEntries([Required][FromQuery] DateTime fromDate, [FromQuery] string secret)
+        {
+            var json = StartAsyncTaskAndGetJson(() =>
+                _integrationProvider.RefreshEntriesToDateAsync(fromDate),
+                nameof(RefreshRecentEntries),
+                secret);
+            return Json(json);
+        }
+
+        [HttpGet("players/{playerId}/entries/refresh")]
+        public JsonResult RefreshPlayerEntries([FromRoute] long playerId, [FromQuery] string secret)
+        {
+            var json = StartAsyncTaskAndGetJson(() =>
+                _integrationProvider.RefreshPlayerEntriesAsync(playerId),
+                nameof(RefreshPlayerEntries),
+                secret);
+            return Json(json);
+        }
+
+        [HttpGet("current-action")]
+        public JsonResult GetCurrentAction([FromQuery] string secret)
+        {
+            var json = StartAsyncTaskAndGetJson<object>(null, null, secret);
+            return Json(json);
+        }
+
+        private object StartAsyncTaskAndGetJson<T>(Func<Task<T>> asyncCall, string taskName, string secret)
+        {
+            if (!_secret.Equals(secret, StringComparison.Ordinal))
+            {
+                return new { msg = "You're not allowed to do this action." };
+            }
+
+            object returned = null;
+            lock (_lock)
+            {
+                if (_runningTask != null)
+                {
+                    if (!_runningTask.IsCompleted)
+                    {
+                        returned = new { msg = $"Task \"{_taskName}\" is already running." };
+                    }
+                    else
+                    {
+                        returned = new { msg = $"Task \"{_taskName}\" just finished.", result = _taskResult };
+                        _runningTask = null;
+                        _taskName = null;
+                    }
+                }
+                else
+                {
+                    if (asyncCall != null)
+                    {
+                        _taskName = taskName;
+                        _runningTask = Task.Run(async () =>
+                        {
+                            try
+                            {
+                                _taskResult = await asyncCall().ConfigureAwait(false);
+                            }
+                            catch (Exception ex)
+                            {
+                                _taskResult = new { globalTaskError = $"{ex.Message}\n{ex.StackTrace}" };
+                            }
+                        });
+                        returned = new { msg = $"Task \"{_taskName}\" has been launched." };
+                    }
+                    else
+                    {
+                        returned = new { msg = "Nothing is currently running." };
+                    }
+                }
+            }
+            return returned;
+        }
+    }
+}
